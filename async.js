ac2938317508dda42288356ecf86989463197928
Hamid <syedhamid47@gmail.com>
2020-07-21 11:14:17 -0400

    day 7

--- async.js
+++ async.js
@@ -1,7 +1,25 @@
-async function fn( ){
-    await setTimeout(() => {console.log("timer over")}, 2000); 
-    console.log("immediately")
-    return 99
+// async function fn( ){
+//     await setTimeout(() => {console.log("timer over")}, 2000); 
+//     console.log("immediately")
+//     return 99
+// }
+// console.log("after async")
+// fn().then((res)=>console.log(res))
+
+
+class Theanable{
+    constructor(num){
+        this.num=num
+    }
+    then(resolve,reject){
+        console.log(resolve)
+
+        setTimeout(() => resolve(this.num*2),1000);
+            
+    }
 }
-console.log("after async")
-fn().then((res)=>console.log(res))
+async function f(){
+    let result =await new Theanable(1);
+    console.log(result);
+
+}
