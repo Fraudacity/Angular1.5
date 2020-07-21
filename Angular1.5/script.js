95febc932114c2840876055551cbcb1bf4b7c315
Hamid <syedhamid47@gmail.com>
2020-07-21 15:52:54 -0400

    day 7 #4

--- Angular1.5/script.js
+++ Angular1.5/script.js
@@ -1,3 +1,25 @@
 //ccreate a module 
 
-var myApp = angular
+var myApp = angular.module("myModule", []);
+
+myApp.controller('myController', function($scope){
+    $scope.message ="AngularJS";
+    $scope.id = 1234;
+})
+ 
+myApp.controller('myController2', function($scope){
+    $scope.message ="AngularJS";
+    $scope.id = 1234;
+})
+ 
+
+myApp.controller('myController3', function($scope){
+   var employee = {
+       firstName:"John",
+       lastName:"Wick",
+       city:"NJ",
+       flag:'flag.jpg',
+   }
+   $scope.emp =employee;
+})
+ 
