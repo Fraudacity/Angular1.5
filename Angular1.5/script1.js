4fdf9d305c8108197dc23d95e7f5ee92b9eab152
Hamid <syedhamid47@gmail.com>
2020-07-22 14:00:27 -0400

    day 8th

--- Angular1.5/script1.js
+++ Angular1.5/script1.js
@@ -1,5 +1,28 @@
-var myApp = angular.modile("myModule", []);
+// 
 
-myApp.controller('myController', function($scope){
-    $scope.message = "angularJS from tcontroller 1"
-}
+var app = angular 
+.module("myModule", [])
+.controller("myController1", function($scope){
+    // var person=[
+    //     {firstName:"John", lastName:"Wick", city:""}, 
+    //     {firstName:"John", lastName:"Wick", city:""},
+    //     {firstName:"John", lastName:"Wick", city:""}
+    // ];
+    // $scope.persons = persons;
+
+    var technologies = [
+        {name:"Angular", likes:0, dislikes:0},
+        {name:"React", likes:0, dislikes:0},
+        {name:"Vue", likes:0, dislikes:0},
+        {name:"Java", likes:0, dislikes:0},
+        {name:"Python", likes:0, dislikes:0},
+    ];
+    $scope.technologies = technologies;
+
+    $scope.incrementLikes = function(technology){
+        technology.likes++
+    }
+    $scope.incrementDisLikes = function(technology){
+        technology.dislikes++
+    }
+})
